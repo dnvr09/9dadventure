df9fd162a7b83212a75aaf559e0f91552b8092c2
dnvr09 <63107696+dnvr09@users.noreply.github.com>
2020-06-26 17:08:15 +0800

    Update keyboard_control.js

--- keyboard_control.js
+++ keyboard_control.js
@@ -80,12 +80,12 @@ function windblast_activated(){
 	document.getElementById('skill_cast').style.zIndex = "10";
 	document.getElementById('skill_cast_animate').style.visibility ="visible";
 	var skill_cast_delay_fixer = function(){
-		document.getElementById('front').style.visibility = "hidden";
+	document.getElementById('front').style.visibility = "hidden";
 	document.getElementById('back').style.visibility = "hidden";
 	document.getElementById('side1').style.visibility = "hidden";
 	document.getElementById('side2').style.visibility = "hidden";	
 	}
-	setTimeout(skill_cast_delay_fixer,100)
+	setTimeout(skill_cast_delay_fixer,1000)
 	if(top_position != 0){
 		document.getElementById('skill_cast_animate').src ="../../character/avatar_animation/avatar_animation_active/windblast_animation/avatar_animation_back.gif";
 	}
